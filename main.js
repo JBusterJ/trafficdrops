e92dc1b8df5445bf1a75e37dd2792568ca14ea05
JBusterJ <50986806+JBusterJ@users.noreply.github.com>
2022-02-10 17:11:36 +1100

    Update main.js

--- main.js
+++ main.js
@@ -49,18 +49,8 @@ db.collection("traffic").onSnapshot(function (querySnapshot) {
     index++;
     if(index == 1){
         var total = 0;
-        // db.collection("traffic").doc("users").get().then(function (doc) {
-        //     total = doc.data().amount;
-        //     console.log(total + "th visitor has... well... visited the website?");
-        // }).then(function () {
-        //     console.log(total);
-        //     for (var i = 0; i < Math.abs(CURRENT_AMOUNT - total); i++) {
-        //         if (i == Math.abs(CURRENT_AMOUNT - total) - 1) {
-        //             generateDrop();
-        //         }
-        //     };
-        //     CURRENT_AMOUNT = total;
-        // });
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        getPreviousDrops();
     } else{
         index = 0;
         return;
